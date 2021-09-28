3b05083e6115c702eb9b1df6f438a41616b97f91
mushtaq <mushtaqmech.rymec@gmail.com>
2021-09-28 22:33:39 +0530

    first java files being commite

---
+++ A.java
@@ -0,0 +1,17 @@
+class A
+{
+int a=5;
+int b=7;
+void add()
+{
+int c =30,d;
+d= a+b+c;
+System.out.println(d);
+}
+public static void main()
+{
+int e=40,f;
+f =a*b*e;
+System.out.println(f);
+}
+}
