dca5ae66c767c2fd00d859558a7f30719f674a93
mushtaq.ahamad <mushtaq.ahamad@bristlecone.com>
2024-10-15 13:42:23 +0530

    pushing Java latest features

--- src/main/java/org/example/Anagram.java
+++ src/main/java/org/example/Anagram.java
@@ -6,9 +6,13 @@ public class Anagram {
 
     public static void main(String[] args) {
 
-       boolean ana =  isAnagram("anagram","gramana");
+        boolean ana = isAnagram("anagram", "gramana");
 
         System.out.println(ana);
+
+        boolean sil = areAnagrams("silent", "listen");
+        System.out.println(sil);
+
     }
 
     public static boolean isAnagram(String s, String t) {
@@ -19,6 +23,23 @@ public class Anagram {
         Arrays.sort(sArray);
         Arrays.sort(tArray);
 
-        return Arrays.equals(sArray,tArray);
+        return Arrays.equals(sArray, tArray);
+    }
+
+    public static boolean areAnagrams(String str1, String str2) {
+
+        if (str1 != null && str2 != null) {
+            if (str1.length() == str2.length()) {
+                String lowerStr1 = str1.toLowerCase();
+                String lowerStr2 = str2.toLowerCase();
+                char[] s1 = lowerStr1.toCharArray();
+                char[] s2 = lowerStr2.toCharArray();
+
+                Arrays.sort(s1);
+                Arrays.sort(s2);
+                return Arrays.equals(s1, s2);
+            }
+        }
+        return false;
     }
 }
---
+++ src/main/java/org/example/ForLoop.java
@@ -0,0 +1,44 @@
+package org.example;
+
+public class ForLoop {
+    public static void main(String[] args){
+
+//        for (int i = 1; i <=10; i=i+2) {
+//                System.out.println("hello world "+i);
+//            }
+//        System.out.println("-------------");
+//        for (int i= 10; i>=1;i--){
+//            System.out.println(" world "+i);
+//        }
+//
+//        for(int i =2; i<=10; i=i+2){
+//            System.out.printf("%d", i*i).println();
+//        }
+
+//        for (int i=2; i<=20; i=i+2){
+//            System.out.println(i*i);
+//        }
+
+//        for(int i=1; i<=10;i++){
+//            System.out.println((2*i) * (2*i));
+//        }
+
+//        for (int i =10; i>=1; i=i-2){
+//            System.out.println(i*i);
+//        }
+
+//        for(int i =1; i<=10; i=i+2){
+//            System.out.println(i*i);
+//        }
+
+//        for(int i=1;i<=10;i++){
+//            System.out.println("7 * "+ i +" = "+ 7*i);
+//        }
+        int table=7;
+
+        for (int i=1;i<=10;i++){
+            System.out.printf("%d * %d = %d", table, i, table*i).println();
+        }
+
+    }
+}
